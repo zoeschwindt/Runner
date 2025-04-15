7b61d56d565aa1ccb1cd89b18a7836741bb77df3
zoeschwindt <schwindtzoe380@gmail.com>
2025-04-15 00:13:17 -0300

    Movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    private Rigidbody rd;
+    private bool isDestroyer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rd = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
+    
     {
-        
+        float move = Input.GetAxis("Horizontal");
+
+        rd.linearVelocity = new Vector3(move * moveSpeed * Time.fixedDeltaTime,0,0);
+        if (Input.GetKeyDown(KeyCode.Space) && isDestroyer)
+        {
+            rd.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isDestroyer = false;
+        }
     }
+    
 }
